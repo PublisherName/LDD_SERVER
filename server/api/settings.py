a511623286e26657709fcca5d76b84db17ae60c7
publishername <62213479+PublisherName@users.noreply.github.com>
2023-10-06 00:08:09 +0545

    [Added]: corsheaders

--- server/api/settings.py
+++ server/api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_registration',
+    'corsheaders',
     ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'api.urls'
@@ -130,3 +132,8 @@ REST_REGISTRATION = {
     'REGISTER_EMAIL_VERIFICATION_ENABLED': False,
     'RESET_PASSWORD_VERIFICATION_ENABLED': False,
 }
+
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+)
